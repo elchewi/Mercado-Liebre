4f860ff63473b235a360993821e558bf1d9d5efd
Tomas Montana <montanatomas4@gmail.com>
2023-11-06 16:37:35 -0300

    .

--- src/app.js
+++ src/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'));
-app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/static', express.static(path.join(__dirname, '../public')));
 
-app.listen(3030, () => console.log('Servidor corriendo en http://localhost:3030'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
-app.get("/", (req, res) => {
-    let htmlPath = path.resolve(__dirname, "./views/index.html");
-    res.sendFile(htmlPath)
-})
-
+app.listen(3030, () => {
+    console.log('Servidor corriendo en http://localhost:3030');
+});
