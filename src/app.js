df922231dc2c97b228d641ec7114a164b87ee088
Tomas Montana <montanatomas4@gmail.com>
2023-11-21 15:33:40 -0300

    agregue login y register
    falta estilizar un poco el login y el register

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+
 app.listen(3030, () => {
     console.log('Servidor corriendo en http://localhost:3030');
 });
