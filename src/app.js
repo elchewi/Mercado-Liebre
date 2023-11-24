046899453549a03bafce1443deb25387ab7a40a2
Tomas Montana <montanatomas4@gmail.com>
2023-11-23 21:44:07 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3030;
 
 app.use('/static', express.static(path.join(__dirname, '../public')));
 
@@ -16,6 +17,6 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'register.html'));
 });
 
-app.listen(3030, () => {
-    console.log('Servidor corriendo en http://localhost:3030');
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
